c17d57131c3c10773099caa6adcb789e1c3932a7
David Malchin <malchin459@gmail.com>
2024-05-12 01:29:02 +0300

    Remove unused localProperty function from gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,4 @@
 pluginManagement {
-    val localProperties = java.util.Properties().apply {
-        val file = file("local.properties")
-        if (!file.exists()) file.createNewFile()
-        load(file.inputStream())
-    }
-
-    fun localProperty(key: String): String =
-        localProperties[key] as? String
-            ?: error("Property $key not found in ${file("local.properties").absolutePath}")
-
     val kotlinVersion: String by settings
     val androidPluginVersion: String by settings
 
